1a3f4ee270ad4e0e164942bd4a3d8da47ea7561f
†村神† <CA01971172@st.kawahara.ac.jp>
2023-04-12 12:58:17 +0900

    delete test for release

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
 console.log("Hello World!");
 document.getElementById("root").innerHTML="<div>hoge</div>"
-
-import {DbController} from "./utils/api"
-async function test(){
-    const hogeController:DbController=new DbController("hogeUser012","timetable")
-    await hogeController.updateData({name:"hoge"})
-    await hogeController.readData()
-    console.log(hogeController.data)
-}
-test()
