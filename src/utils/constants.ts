1ab770bdcdc5e087cc68b07aba49b6bbe6a53267
†村神† <CA01971172@st.kawahara.ac.jp>
2023-04-14 09:40:26 +0900

    fix the address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,5 @@
 export const dbUrl:string="https://myschedule-c0a49-default-rtdb.firebaseio.com"
 
 /* ページ関連 */
-export const indexPageUrl:string="/index.html"
-export const loginPageUrl:string="/index.html?page=login"
+export const indexPageUrl:string="./index.html"
+export const loginPageUrl:string="./index.html?page=login"
