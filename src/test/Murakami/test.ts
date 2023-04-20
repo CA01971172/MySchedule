b1bc5963c9765f6afc22ce91706d97e45b9f953e
†村神† <CA01971172@st.kawahara.ac.jp>
2023-04-21 00:58:06 +0900

    cors fix test

--- src/test/Murakami/test.ts
+++ src/test/Murakami/test.ts
@@ -1,7 +1,9 @@
 import {AppUser} from "./../../utils/AppUser"
 import {UserDbController} from "./../../utils/DbController/UserDbController"
+import { ApiDbController } from "../../utils/DbController/ApiDbController"
 import { SendGrid } from "../../utils/SendGrid"
-import { send } from "process"
+import { SendGridSettings,EmailData } from "./../../utils/types"
+import axios from 'axios';
 export async function test(){
     /* リダイレクトやクエリ文字列のテストコード */
 /*     const hogeLink="http://localhost:8080/index.html?page=timetable&mode=edit"
@@ -27,12 +29,49 @@ export async function test(){
     } */
 
     /* メール送信のテストコード */
-    const sendGrid: SendGrid = new SendGrid()
+/*     const sendGrid: SendGrid = new SendGrid()
     await sendGrid.setAPIKey()
     sendGrid.sendEmail({
         to: "sorera620@gmail.com",
         bcc:"",
         subject:"hoge",
         text:"This is hoge."
-    })
+    }) */
+
+    const dbController: ApiDbController = new ApiDbController("SendGrid")
+    const sendGridSettings: SendGridSettings = await dbController.readData() as SendGridSettings
+    const APIKey = sendGridSettings.APIKey
+    const fromEmail = sendGridSettings.fromEmail
+
+    const sendEmail = async (data:any) => {
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${APIKey}`,
+            'Access-Control-Allow-Origin': '*'
+        };
+        const url = 'https://cors-anywhere.herokuapp.com/https://api.sendgrid.com/v3/mail/send';
+        const response = await axios.post(url, data, { headers });
+        console.log(response.data);
+    }
+
+    const data = {
+        personalizations: [
+            {
+                to: [{ email: 'hoge@gmail.com' }]
+            }
+        ],
+        from: {
+            email: fromEmail,
+            name: 'Example'
+        },
+        subject: 'Hello, world!',
+        content: [
+            {
+                type: 'text/plain',
+                value: 'Hello, world!'
+            }
+        ]
+    };
+
+    await sendEmail(data);
 }
