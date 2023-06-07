9b3a4772baed567a55c70442576dcee457c64d47
†村神† <CA01971172@st.kawahara.ac.jp>
2023-06-07 09:40:50 +0900

    Implement Login

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
+import AppUser from "./../../utils/AppUser"
+import { IndexPageUrl } from "../../utils/constants";
 
+// ログイン用フォーム
 export default function LoginForm() {
-    const [hidePassword, setHidePassword] = useState<boolean>(true);
+    // フォームの状態を管理する
+    const [emailValue, setEmailValue] = useState<string>(""); // メールアドレスの値を管理する
+    const [passwordValue, setPasswordValue] = useState<string>(""); // パスワードの値を管理する
+    const [hidePassword, setHidePassword] = useState<boolean>(true); // パスワードの表示錠チアを管理する
 
     return (
         <form className="m-2 p-3 border rounded">
@@ -9,16 +15,18 @@ export default function LoginForm() {
                 <label
                 className="form-label"
                 htmlFor="email"
-                placeholder="メールアドレスを入力"
                 >
                     メールアドレス
                 </label>
                 <input
                 className="form-control"
                 type="email"
+                placeholder="メールアドレスを入力"
                 name="email"
                 id="email"
                 required={false}
+                value={passwordValue}
+                onChange={(event) => setPasswordValue(event.target.value)}
                 />
             </div>
             <div className="m-2">
@@ -30,6 +38,8 @@ export default function LoginForm() {
                     placeholder="パスワードを入力(6文字以上)"
                     name="password" id="password" required={false}
                     style={{ borderRadius: "0.25rem" }}
+                    value={emailValue}
+                    onChange={(event) => setEmailValue(event.target.value)}
                     />
                     <span
                     className={`search-clear ${(hidePassword ? "bi bi-eye-fill" : "bi bi-eye-slash-fill")}`}
@@ -41,6 +51,11 @@ export default function LoginForm() {
             <button
             className="m-2 btn btn-outline-primary"
             type="submit"
+            onClick={(event) => {
+                event.preventDefault();
+                AppUser.setUserInfo(emailValue, passwordValue);
+                AppUser.signIn(IndexPageUrl);
+            }}
             >
                 ログイン
             </button>
