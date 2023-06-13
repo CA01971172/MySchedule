04e175d3cb0814e9983a65e352fbb7b94c82c798
†村神† <CA01971172@st.kawahara.ac.jp>
2023-06-13 12:13:50 +0900

    add DbController

---
+++ src/components/HamburgerMenu/EventHamburgerMenu.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { EventSettingsDbController } from '../../utils/DbController/EventSettingsDbController';
+
+export default function EventHamburgerMenu() {
+    return (
+        <div/>
+    );
+}
