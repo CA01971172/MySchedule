134d366d47b705a455e045658a6dbd1b66b507eb
†村神† <CA01971172@st.kawahara.ac.jp>
2023-06-13 11:17:15 +0900

    design Cards

---
+++ src/components/Card/TandemCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Task, Event } from "./../../utils/types"
+
+export default function TandemCard({ id, data }: { id: string, data: Task|Event}) {
+    return (
+        <div/>
+    );
+}
---
+++ src/components/Card/TimetableCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Timetable } from "./../../utils/types"
+
+export default function TimetableCard({ id, data }: { id: string, data: Timetable}) {
+    return (
+        <div/>
+    );
+}
