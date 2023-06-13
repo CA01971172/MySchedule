be380a63acf6864d25d62b12c4c9ece176bbf5d2
†村神† <CA01971172@st.kawahara.ac.jp>
2023-06-13 12:28:47 +0900

    update

--- src/components/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/HamburgerMenu/HamburgerMenu.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import HamburgerMenuHeader from './HamburgerMenuHeader';
+import TaskHamburgerMenu from './TaskHamburgerMenu';
+import ShiftHamburgerMenu from './ShiftHamburgerMenu';
+import EventHamburgerMenu from './EventHamburgerMenu';
+import { TabType } from '../../utils/types';
 
-export default function HamburgerMenu() {
+export default function HamburgerMenu({ tabType }: { tabType: TabType }) {
     return (
         <div/>
     );
