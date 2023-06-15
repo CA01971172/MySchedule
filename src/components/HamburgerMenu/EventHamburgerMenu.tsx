6cf43e2ad6e8273a4c68d6979aa372e5aaf688f8
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-06-15 12:31:55 +0900

    Fixed the indentation of EventHamburgerMenu

--- src/components/HamburgerMenu/EventHamburgerMenu.tsx
+++ src/components/HamburgerMenu/EventHamburgerMenu.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-export  function EventHamburgerMenu() {
+export function EventHamburgerMenu() {
   const [checkboxes, setCheckboxes] = useState([false, false]);
   const [numberInput, setNumberInput] = useState('');
 
@@ -9,60 +9,60 @@ export  function EventHamburgerMenu() {
       checkboxes.map((isChecked, i) => (i === index ? !isChecked : isChecked))
     );
   };
-  const handleNumberInputChange = (event:any) => {
+
+  const handleNumberInputChange = (event: any) => {
     setNumberInput(event.target.value);
   };
-  return (
-    
-      <main>
-    <div>
-      <div className="d-flex justify-content-between border-bottom border-secondry">
-        <button className="btn">×</button>
-        <button type="button" className="btn btn-outline-danger">Logout</button>
-    </div>
 
-    <div className="form-check">
-      <input
-        className="form-check-input"
-        type="checkbox"
-        value=""
-        id="flexCheckDefault"
-        checked={checkboxes[0]}
-        onChange={() => toggleCheckbox(0)}
-      />
-      <label className="form-check-label" htmlFor="flexCheckDefault">
-        提出期限が迫ったら、アラートメールで通知する
-      </label>
-    </div>
-       
+  return (
+    <main>
+      <div>
+        <div className="d-flex justify-content-between border-bottom border-secondary">
+          <button className="btn">×</button>
+          <button type="button" className="btn btn-outline-danger">Logout</button>
+        </div>
+
+        <div className="form-check">
           <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault1"
+            checked={checkboxes[0]}
+            onChange={() => toggleCheckbox(0)}
+          />
+          <label className="form-check-label" htmlFor="flexCheckDefault1">
+            提出期限が迫ったら、アラートメールで通知する
+          </label>
+        </div>
+
+        <input
           type="number"
           name="example1"
           value={numberInput}
           onChange={handleNumberInputChange}
         />
 
-<div className="form-check">
-      <input
-        className="form-check-input"
-        type="checkbox"
-        value=""
-        id="flexCheckDefault"
-        checked={checkboxes[1]}
-        onChange={() => toggleCheckbox(1)}
-      />
-      <label className="form-check-label" htmlFor="flexCheckDefault">
-        提出期限を過ぎた課題を自動で削除する
-      </label>
-    </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault2"
+            checked={checkboxes[1]}
+            onChange={() => toggleCheckbox(1)}
+          />
+          <label className="form-check-label" htmlFor="flexCheckDefault2">
+            提出期限を過ぎた課題を自動で削除する
+          </label>
+        </div>
 
-        <button  className="btn btn-outline-dark" style={{ display: 'block', margin: '0 auto' }}>
+        <button className="btn btn-outline-dark" style={{ display: 'block', margin: '0 auto' }}>
           一括削除
         </button>
-        </div>
-      </main>
-    
+      </div>
+    </main>
   );
 }
 
-export default  EventHamburgerMenu;
+export default EventHamburgerMenu;
