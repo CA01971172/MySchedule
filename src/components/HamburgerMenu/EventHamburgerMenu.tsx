cc340b94c4deee92af7cda058d42f4c962d3f796
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-06-15 11:13:24 +0900

    add EventHamburgerMenu

--- src/components/HamburgerMenu/EventHamburgerMenu.tsx
+++ src/components/HamburgerMenu/EventHamburgerMenu.tsx
@@ -1,31 +1,68 @@
 import React, { useState } from 'react';
-import { Button, Offcanvas } from "react-bootstrap";
-// 使用していない
-import EventSettingsDbController from '../../utils/DbController/EventSettingsDbController';
-import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function EventHamburgerMenu() {
-    const [show, setShow] = useState(false);
+function App() {
+  const [checkboxes, setCheckboxes] = useState([false, false]);
+  const [numberInput, setNumberInput] = useState('');
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    return (
-        <>
-            <Button variant="primary" onClick={handleShow}>
-                <i className="fa fa-bars"></i>
-            </Button>
-
-            <Offcanvas show={show} onHide={handleClose} placement='end'>
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Menu</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                    <p>
-                        You can place your menu items here.
-                    </p>
-                </Offcanvas.Body>
-            </Offcanvas>
-        </>
+  const toggleCheckbox = (index: number) => {
+    setCheckboxes(
+      checkboxes.map((isChecked, i) => (i === index ? !isChecked : isChecked))
     );
+  };
+  const handleNumberInputChange = (event:any) => {
+    setNumberInput(event.target.value);
+  };
+  return (
+    
+      <main>
+    <div>
+      <div className="d-flex justify-content-between border-bottom border-secondry">
+        <button className="btn">×</button>
+        <button type="button" className="btn btn-outline-danger">Logout</button>
+    </div>
+
+    <div className="form-check">
+      <input
+        className="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckDefault"
+        checked={checkboxes[0]}
+        onChange={() => toggleCheckbox(0)}
+      />
+      <label className="form-check-label" htmlFor="flexCheckDefault">
+        提出期限が迫ったら、アラートメールで通知する
+      </label>
+    </div>
+       
+          <input
+          type="number"
+          name="example1"
+          value={numberInput}
+          onChange={handleNumberInputChange}
+        />
+
+<div className="form-check">
+      <input
+        className="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckDefault"
+        checked={checkboxes[1]}
+        onChange={() => toggleCheckbox(1)}
+      />
+      <label className="form-check-label" htmlFor="flexCheckDefault">
+        提出期限を過ぎた課題を自動で削除する
+      </label>
+    </div>
+
+        <button  className="btn btn-outline-dark" style={{ display: 'block', margin: '0 auto' }}>
+          一括削除
+        </button>
+        </div>
+      </main>
+    
+  );
 }
+
+export default App;
