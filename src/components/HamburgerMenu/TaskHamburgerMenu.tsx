bd461401a5f59cb52b7df64be36688ce820d6341
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-06-15 12:25:11 +0900

    add TaskHamburgerMenu

--- src/components/HamburgerMenu/TaskHamburgerMenu.tsx
+++ src/components/HamburgerMenu/TaskHamburgerMenu.tsx
@@ -1,8 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { TaskSettingsDbController } from '../../utils/DbController/TaskSettingsDbController';
+import React, { useState } from 'react';
 
-export default function TaskHamburgerMenu() {
-    return (
-        <div/>
+export function TaskHamburgerMenu() {
+  const [checkboxes, setCheckboxes] = useState([false, false]);
+  const [numberInput, setNumberInput] = useState('');
+
+  const toggleCheckbox = (index: number) => {
+    setCheckboxes(
+      checkboxes.map((isChecked, i) => (i === index ? !isChecked : isChecked))
     );
+  };
+
+  const handleNumberInputChange = (event: any) => {
+    setNumberInput(event.target.value);
+  };
+
+  return (
+    <main>
+      <div>
+        <div className="d-flex justify-content-between border-bottom border-secondary">
+          <button className="btn">×</button>
+          <button type="button" className="btn btn-outline-danger">Logout</button>
+        </div>
+
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+            checked={checkboxes[0]}
+            onChange={() => toggleCheckbox(0)}
+          />
+          <label className="form-check-label" htmlFor="flexCheckDefault">
+            過去の予定を非表示にする
+          </label>
+        </div>
+
+        <button className="btn btn-outline-dark" style={{ display: 'block', margin: '0 auto' }}>
+          一括削除
+        </button>
+      </div>
+    </main>
+  );
 }
+
+export default TaskHamburgerMenu;
