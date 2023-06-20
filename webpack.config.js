edabd460e7a479d3fcd4666e856756cf107c87de
†村神† <CA01971172@st.kawahara.ac.jp>
2023-06-20 15:10:34 +0900

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 //const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 
-// PWAの設定
-const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
-const outputPath = path.resolve(__dirname, "dist");
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-
 const isProduction = process.env.NODE_ENV == "production";
 
 module.exports = {
@@ -45,24 +40,6 @@ module.exports = {
       filename: 'style.css'
     }),
     //new FixStyleOnlyEntriesPlugin(),
-
-    // PWAの設定
-    new WebpackPwaManifest({
-      short_name: "MySchedule",
-      name: "MySchedule",
-      display: "standalone",
-      start_url: "index.html",
-      background_color: "#0d6efd",
-      theme_color: "#0d6efd",
-      icons: [{
-        src: path.resolve("dist/MySchedule.png"),
-        sizes: [96, 128, 192, 256, 384, 512],
-      }]
-    }),
-    new WorkboxWebpackPlugin.GenerateSW({
-      swDest: outputPath + "/service-worker.js",
-      maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MBまでキャッシュする
-    })
   ],
   module: {
     rules: [
