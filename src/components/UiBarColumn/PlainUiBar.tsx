a27ccd7f114005acb6c19042fb831a62f16952c6
†村神† <CA01971172@st.kawahara.ac.jp>
2023-06-27 09:38:38 +0900

    Implement gear icon

--- src/components/UiBarColumn/PlainUiBar.tsx
+++ src/components/UiBarColumn/PlainUiBar.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import HamburgerMenuButton from "./HamburgerMenuButton"
+import SettingsButton from "./SettingsButton"
 
 export default function PlainUiBar(){
     return (
         <div className="row border-bottom">
             <div className="col p-0">
                 <HamburgerMenuButton/>
+            </div>
+            <div className="col-auto p-0">
+                <SettingsButton/>
             </div>
         </div>
     );
---
+++ src/components/UiBarColumn/SettingsButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+async function deleteUser(): Promise<void>{
+
+}
+
+export default function SettingsButton() {
+    return (
+        <button
+            className="btn fs-3"
+            onClick={() => {
+
+            }}
+        >
+            <i className="bi bi-gear-fill"/>
+        </button>
+    );
+}
