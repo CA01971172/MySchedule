d981d12f7df67b310f246fa93821cc1c2ee27a56
†村神† <CA01971172@st.kawahara.ac.jp>
2023-09-21 13:36:46 +0900

    add test code

--- src/index.tsx
+++ src/index.tsx
@@ -47,23 +47,23 @@ window.onload = async function(){
 
 
 //テスト用コード(コンソール上で実行)
-// declare global {
-//     interface Window {
-//         hoge(): void;
-//     }
-// }
-// window.hoge = async function (): Promise<void>{
-//     const serverAddress: string = "https://myschedule-server.iti2022kawahara.com";
-//     try{
-//         const serverLink: string = `${serverAddress}`
-//         const response = await fetch(serverLink, {
-//             method: 'GET',
-//             mode: 'cors',
-//         });
-//         const result = await response.text();
-//         console.log(result);
-//     }catch(e){
-//         console.log(e);
-//         throw new Error("サーバーへの接続に失敗しました");
-//     }
-// }
+declare global {
+    interface Window {
+        testConnect(): void;
+    }
+}
+window.testConnect = async function (): Promise<void>{
+    const serverAddress: string = "https://myschedule-server.iti2022kawahara.com";
+    try{
+        const serverLink: string = `${serverAddress}`
+        const response = await fetch(serverLink, {
+            method: 'GET',
+            mode: 'cors',
+        });
+        const result = await response.text();
+        console.log(result);
+    }catch(e){
+        console.log(e);
+        throw new Error("サーバーへの接続に失敗しました");
+    }
+}
